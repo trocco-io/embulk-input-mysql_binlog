50a870a79796136e140623d061039371e6d345c0
giwa <ugw.gi.world@gmail.com>
2019-11-29 13:24:51 +0900

    config unit test

--- src/test/java/org/embulk/input/mysql_binlog/TestMysqlBinlogInputPlugin.java
+++ src/test/java/org/embulk/input/mysql_binlog/TestMysqlBinlogInputPlugin.java
@@ -1,5 +1,39 @@
 package org.embulk.input.mysql_binlog;
 
+import org.embulk.config.ConfigSource;
+import org.embulk.spi.InputPlugin;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestMysqlBinlogInputPlugin
 {
+    private ConfigSource config;
+    private static final String BASIC_RESOURCE_PATH = "org/embulk/input/mysql_binlog/";
+    private static ConfigSource loadYamlResource(TestingEmbulk embulk, String fileName) {
+        return embulk.loadYamlResource(BASIC_RESOURCE_PATH + fileName);
+    }
+    @Rule
+    public TestingEmbulk embulk = TestingEmbulk.builder()
+            .registerPlugin(InputPlugin.class, "mysql_binlog", MysqlBinlogInputPlugin.class)
+            .build();
+
+    @Test
+    public void checkBasicConfig() {
+        config = loadYamlResource(embulk, "base.yml");
+        PluginTask task = config.loadConfig(PluginTask.class);
+        assertEquals("localhost", task.getHost());
+        assertEquals(3306, task.getPort());
+        assertEquals("test", task.getDatabase());
+        assertEquals("test", task.getTable());
+        assertEquals("root", task.getUser());
+        assertEquals("root", task.getPassword());
+        assertEquals("mysql-bin.000001", task.getFromBinlogFilename());
+        assertEquals(Long.valueOf(4), task.getFromBinlogPosition());
+        assertTrue(task.getEnableMetadata());
+        assertEquals("_trocco_", task.getMetadataPrefix());
+        assertNotNull(task.getColumns());
+    }
 }
